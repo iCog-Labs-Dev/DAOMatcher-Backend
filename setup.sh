26f1afa19b5675cbd47e30078f4690a8b0409535
Yab <yabuman46@gmail.com>
2023-08-24 13:32:34 +0300

    Created a script to setup the servers

--- setup.sh
+++ setup.sh
@@ -1,13 +1,35 @@
+#!/bin/bash
+
+cleanup() {
+    echo  ""
+    echo "Cleaning up..."
+    if [ -n "$llm_pid" ]; then
+        kill -TERM "$llm_pid"
+    fi
+    if [ -n "$app_pid" ]; then
+        kill -TERM "$app_pid"
+    fi
+    deactivate
+    echo "Servers Terminated"
+    exit 0
+}
+
+trap cleanup SIGINT
+
 echo "Setting up environment"
 
 python3 -m venv Backend
-source Backend/bin/activate
+source Backend/bin/activate     
 
 echo "Installing requirements"
-pip install -r requirements.txt
+pip -q install -r requirements.txt
 
-echo "Starting LLM server"
-python3 -m src.LLM.LLMServer.py &
+echo "Starting LLM server on port 5001"
+python3 -m src.LLM.LLMServer.py & 
+llm_pid=$!
 
-echo "Starting Up app server"
-python3 app.py
+echo "Starting App server on port 5000"
+python3 app.py &
+app_pid=$!
+
+wait
