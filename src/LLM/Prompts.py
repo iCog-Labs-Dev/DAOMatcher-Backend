533eec660d24fcc24d4cd3274eef7235fe1810e4
Yab <yabuman@gmail.com>
2023-08-30 13:24:00 +0300

    First attempt to fix prompt

--- src/LLM/Prompts.py
+++ src/LLM/Prompts.py
@@ -1,17 +1,15 @@
 DEFAULT_SYSTEM_PROMPT = """
-Given a person who is described as follows, display a single number denoting how much the person would be interested in the given topic from 1-100. If there is no indication that the person is not interested in the query, output 0. If there are things that indicate the person is absolutly interested on the subject output 100. You can also output a single digit between 1 and 100 to indicate how likely the person is to be interested in the topic based on the given description. Please only output a single number and nothing else. DO NOT explain anything, Just output the number.
+  You are expert human topic interest inferer.
+  You are given a summarized paragraphs of posts below in a triple back ticks.
+  Follow the following step and rate the persons interest in the topic given below in the angular brackets on the scale of 100.
+  Your output should only contain a single number.
   """
-# This prompt doens't work as expected
-"You are a machine that can understand human language but only speak using numbers. You can't display any other characters but digits. You also can understand human's interests given a simple description about them. Your task is to output a single number between 1-100 corresponding to a person's interest on a given topic after you are given a description about that person."
-
-# This prompt doens't work as expected
-"You are a professional interest analyzer who specializes at giving a corresponding number from 1-100 for a person's interest on a given topic given the perons's one paragraph description. Your job is to output a single number without any further explanation. If the person seems to be interested in the topic very much, output 100, if the person isn't interested in the topic at all out put 0. You can also output single digit number corresponding to the person that is between 1-100."
-
-# This prompt works fine as long as a good description is given
+#The current working prompt
 "Given a person who is described as follows, display a single number denoting how much the person would be interested in the given topic from 1-100. If there is no indication that the person is not interested in the query, output 0. If there are things that indicate the person is absolutly interested on the subject output 100. You can also output a single digit between 1 and 100 to indicate how likely the person is to be interested in the topic based on the given description. Please only output a single number and nothing else. DO NOT explain anything, Just output the number."
 
 SUMMERIZE_SYSTEM_PROMPT = """
-    Given lists of posts, you job is to shorten each post into a smaller number of sentences. You need to summerize each and every post separately and join them into the same paragraph in your response. You should only respond with the final result and nothing else.
+    Given the following paragraphs separated by **** in a triple backticks, summarize each paragraph using excalty 50 words.
+    Your response should contain all the paragraphs separated by **** with no other additional infomation attached to it.
   """
 
 B_INST, E_INST = "[INST]", "[/INST]"
@@ -21,6 +19,10 @@ B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
 S_SYSTEM_PROMPOT = B_SYS + SUMMERIZE_SYSTEM_PROMPT + E_SYS
 
-INSTRUCTION = "How much is the person interested in the topic of {query} given the following description/n Description: {content}"
+INSTRUCTION = """How much is the person interested in the following topic separated by angular brackets given the following posts in triple backticks.
+topic: <{query}>
+Description: ```{content}```
+Response: <single number from 1-100>
+"""
 S_INSTRUCTION = """Shorten each of the following posts that are put into separate paragraph. Summerize each post without losing its context and topic into a single sentence. You should responde with a single long paragraph that has all summeries about the posts.
 Posts: {content}"""
