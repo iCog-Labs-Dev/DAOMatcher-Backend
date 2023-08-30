ce38de30b3cea11e814bd550da58ca2e22cb31aa
Yab <yabuman@gmail.com>
2023-08-30 14:11:50 +0300

    Found a descent prompt that works well

--- src/LLM/Prompts.py
+++ src/LLM/Prompts.py
@@ -7,8 +7,7 @@ DEFAULT_SYSTEM_PROMPT = """
 "Given a person who is described as follows, display a single number denoting how much the person would be interested in the given topic from 1-100. If there is no indication that the person is not interested in the query, output 0. If there are things that indicate the person is absolutly interested on the subject output 100. You can also output a single digit between 1 and 100 to indicate how likely the person is to be interested in the topic based on the given description. Please only output a single number and nothing else. DO NOT explain anything, Just output the number."
 
 SUMMERIZE_SYSTEM_PROMPT = """
-    Given the following paragraphs separated by '------------------' in a triple backticks, summarize each paragraph using excalty 50 words.
-    Your response should contain all the paragraphs separated by '------------------' with no other additional infomation attached to it.
+    Given the following paragraphs separated by '------------------' in a triple backticks, summarize each paragraph using at most 50 words.
   """
 
 B_INST, E_INST = "[INST]", "[/INST]"
@@ -19,10 +18,10 @@ SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
 S_SYSTEM_PROMPOT = B_SYS + SUMMERIZE_SYSTEM_PROMPT + E_SYS
 
 INSTRUCTION = """For the following topic in '<>' and description in '``````' perform the following steps
-step 1: analyze whether the given description is related to the topic or not
-step 2: if the description is related, predict the description relation to the topic on the scale of 1-100
-step 3: if the description is not related, predict the description relation to the topic as 0
-step 4: ouput a single number based on your analysis
+step 1: from the given description about a user, identify sentences that are related to the given topic
+step 2: if you can find sentences related to the provided topics procced with step 3 other wise output 0 and end your analysis.
+step 3: rate how much interested can the user be on the given topic on the scale of 1-100 based on the identified sentences and their correlation with the topic.
+step 4: ouput a single number based on step 2.
 
 topic: <{query}>
 Description: ```{content}```
@@ -30,5 +29,14 @@ Description: ```{content}```
 Strictly provide your output as follows. Don't include any explanation or any text.
 Response: <single number from 1-100>
 """
-S_INSTRUCTION = """Shorten each of the following posts that are put into separate paragraph. Summerize each post without losing its context and topic into a single sentence. You should responde with a single long paragraph that has all summeries about the posts.
-Posts: {content}"""
+S_INSTRUCTION = """
+For the posts provided in triple backticks, summarize each post in using at most 50 words.
+Each posts are separated using '------------------', so your output should also be separated by '------------------'
+corresponding to each summarized posts.
+
+Posts: ```{content}```
+
+Use the following format for the output.
+Response: <Summarized Post 1.------------------Summarized Post 2.------------------>
+
+"""
