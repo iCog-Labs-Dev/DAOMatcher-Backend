7fd82e0d49da8bd68541c7c592e53dabe5eb3dc6
Yab <yabuman@gmail.com>
2023-08-30 14:32:44 +0300

    Removed unused blocks of codes

--- src/LLM/LLMMethods.py
+++ src/LLM/LLMMethods.py
@@ -43,15 +43,7 @@ def remove_substring(string, substring):
 
 
 def generate(query, content, llm):
-    # s_prompt = get_prompt(S_INSTRUCTION, S_SYSTEM_PROMPOT)
     prompt = get_prompt(INSTRUCTION)
-    # print(prompt)
-
-    # s_llm_chain = LLMChain(prompt=s_prompt, llm=llm)
-    # s_content: str = s_llm_chain.run(content)
-    # s_content = s_content.replace("\n", "").replace("*", "").split("Response:")[1]
-
-    # print("Short content: ", s_content)
 
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     response = llm_chain.run({"query": query, "content": content})
--- src/LLM/Prompts.py
+++ src/LLM/Prompts.py
@@ -3,19 +3,12 @@ DEFAULT_SYSTEM_PROMPT = """
   If the user seems to be extremly likely to be interested in the topic output 100. If the user doesn't seem to be likely to be interested at all output 0.
   You should never provide any explantion to the output, your response should be only in numbers corresponding to the scores, nothing else.
   """
-#The current working prompt
-"Given a person who is described as follows, display a single number denoting how much the person would be interested in the given topic from 1-100. If there is no indication that the person is not interested in the query, output 0. If there are things that indicate the person is absolutly interested on the subject output 100. You can also output a single digit between 1 and 100 to indicate how likely the person is to be interested in the topic based on the given description. Please only output a single number and nothing else. DO NOT explain anything, Just output the number."
-
-SUMMERIZE_SYSTEM_PROMPT = """
-    Given the following paragraphs separated by '------------------' in a triple backticks, summarize each paragraph using at most 50 words.
-  """
 
 B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
 
 SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
-S_SYSTEM_PROMPOT = B_SYS + SUMMERIZE_SYSTEM_PROMPT + E_SYS
 
 INSTRUCTION = """For the following topic in '<>' and posts separated by '------------------'  in '``````' perform the following steps
 step 1: summarize each post that is separated by '------------------' at most 50 words.
@@ -29,15 +22,4 @@ Description: ```{content}```
 
 Strictly provide your output as follows. Don't include any explanation or any text.
 Response: <single number from 1-100>
-"""
-S_INSTRUCTION = """
-For the posts provided in triple backticks, summarize each post in using at most 50 words.
-Each posts are separated using '------------------', so your output should also be separated by '------------------'
-corresponding to each summarized posts.
-
-Posts: ```{content}```
-
-Use the following format for the output.
-Response: <Summarized Post 1.------------------Summarized Post 2.------------------>
-
 """
