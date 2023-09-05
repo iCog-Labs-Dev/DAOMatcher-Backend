4c1d147cde666086a1f033516b6d8dfdbc53ca3d
Yab <yabuman@gmail.com>
2023-09-05 13:57:17 +0300

    Refactored the server logic

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify, abort
-from src.ServerLogic.StoringUsers import *
+from src.ServerLogic.StoringUsers import StoreUsers
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
 LOCAL_APP_PORT = 5000
+storeUsers = StoreUsers()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -15,32 +17,39 @@ def scoring_user():
         return "Send post request"
     elif request.method == "POST":
         jsonRequest = request.json
-        
-        if not all(key in jsonRequest for key in ("query", "user_list", "user_limit")):
-            abort(400, description="Invalid JSON request. Make sure the request has set 'query', 'user_list' and 'user_limit'")
 
-        
+        if not all(key in jsonRequest for key in ("query", "user_list", "user_limit")):
+            abort(
+                400,
+                description="Invalid JSON request. Make sure the request has set 'query', 'user_list' and 'user_limit'",
+            )
+
         query = request.json["query"]
         user_list = request.json["user_list"]
         user_limit = request.json["user_limit"]
         try:
-            result = scour(user_list, query, user_limit)
+            result = storeUsers.scour(user_list, query, user_limit)
             users = []
             for user in result:
-                score , handle, userInfo = user
-                users.append({
-                    "id": userInfo["id"],
-                    "username": userInfo["username"],
-                    "name": userInfo["name"],
-                    "score": score,
-                    "handle": handle
-                })
+                score, handle, userInfo = user
+                users.append(
+                    {
+                        "id": userInfo["id"],
+                        "username": userInfo["username"],
+                        "name": userInfo["name"],
+                        "score": score,
+                        "handle": handle,
+                    }
+                )
 
             print(f"result: {users}")
             data = {"result": users}
             return jsonify(data)
         except requests.exceptions.RequestException as e:
-            abort(e.response.status_code, description=f"Invalid response submitted to the LLM server")
+            abort(
+                e.response.status_code,
+                description=f"Invalid response submitted to the LLM server",
+            )
         except Exception as e:
             abort(500, description="Server encountered Unknown error")
 
--- src/ServerLogic/StoringUsers.py
+++ src/ServerLogic/StoringUsers.py
@@ -4,110 +4,110 @@ import requests
 from src.ServerLogic import mastodon, linkedIn, llm_server
 
 
-def store_items(item, limit, user_heap):
-    if len(user_heap) == limit:
-        heappushpop(user_heap, item)
-    else:
-        heappush(user_heap, item)
-
-
-# Returns Mastodon user content and user in a dictionary of keys id, name, username
-def get_mastodon_user(acc, server):
-    profile = mastodon.getProfile(server, acc)
-    if profile:
-        content = []
-        # print(id)
-        if "note" in profile:
-            content.append(mastodon.extractText(profile["note"]))
-            # print(content[-1])
-        for c in mastodon.getContent(server, profile["id"]):
-            if "content" in c and c["content"]:
-                content.append(mastodon.extractText(c["content"]))
-                # print(content[-1])
-        content = "\n\n------------------\n".join(content)
-        user = {
-            "id": profile["id"],
-            "name": profile["display_name"],
-            "username": profile["username"],
-        }
-        # print(content)
-        return content, user
-    return None, None
-
-
-# Returns LinkedIn user content and user in a dictionary of keys id, name, username
-def get_linkedIn_user(username):
-    profile = linkedIn.getLinkedInProfile(username)
-
-    if profile:
-        content = []
-
-        if "aboutSummaryText" in profile and profile["aboutSummaryText"]:
-            content.append(profile["aboutSummaryText"])
-
-        for p in linkedIn.getUserPosts(profile):
-            if "text" in p and p["text"]:
-                content.append(p["text"])
-
-        content = "\n\n------------------\n".join(content)
-        saleNavId = linkedIn.getSaleNavId(profile["salesNavLink"])
-        username = linkedIn.getUsername(profile["link"])
-
-        user = {"id": saleNavId, "name": profile["name"], "username": username}
-
-        return content, user
-    return None, None
-
-
-def scour(starting_users, query, user_limit):
-    user_heap = []
-
-    accounts = deque(starting_users)
-
-    visited = set()
-    count = 0
-
-    while accounts and count < user_limit:
-        account = accounts.popleft()
-        if (
-            "@" in account
-        ):  # If it contains @ it is mastodon otherwise it is LinkedIn URL
-            _, acc, server = account.split("@")
-            content, user = get_mastodon_user(acc, server)
-
-            # If there is no user found, no point in excuting the rest of the code
-            if not user:
-                continue
-
-            # Get mastodon followers
-            for follower in mastodon.getFollowers(server, user["id"]):
-                username = follower["acct"]
-                if "@" in username:
-                    username = "@" + username
-                else:
-                    username = "@" + username + "@" + server
-                if username not in visited:
-                    accounts.append(username)
-                    visited.add(username)
+class StoreUsers:
+    def __store_items(self, item, limit, user_heap):
+        if len(user_heap) == limit:
+            heappushpop(user_heap, item)
         else:
-            content, user = get_linkedIn_user(account)
+            heappush(user_heap, item)
 
-            # Get followers for linkedIn
-            for follower in linkedIn.getConnections(account, 1000):
-                username = follower["publicIdentifier"]
-                if username not in visited:
-                    accounts.append(username)
-                    visited.add(username)
+    # Returns Mastodon user content and user in a dictionary of keys id, name, username
+    def __get_mastodon_user(self, acc, server):
+        profile = mastodon.getProfile(server, acc)
+        if profile:
+            content = []
+            # print(id)
+            if "note" in profile:
+                content.append(mastodon.extractText(profile["note"]))
+                # print(content[-1])
+            for c in mastodon.getContent(server, profile["id"]):
+                if "content" in c and c["content"]:
+                    content.append(mastodon.extractText(c["content"]))
+                    # print(content[-1])
+            content = "\n\n------------------\n".join(content)
+            user = {
+                "id": profile["id"],
+                "name": profile["display_name"],
+                "username": profile["username"],
+            }
+            # print(content)
+            return content, user
+        return None, None
 
-        if user:
-            try:
-                score = llm_server.generate_search(query, content)["response"]
-                store_items(((int(score), account, user)), user_limit, user_heap)
-                # print(count)
-                count += 1
-            except requests.exceptions.RequestException as e:
-                raise e
-            except Exception as e:
-                raise Exception("Error encountered on storing the scores")
+    # Returns LinkedIn user content and user in a dictionary of keys id, name, username
+    def __get_linkedIn_user(self, username):
+        profile = linkedIn.getLinkedInProfile(username)
 
-    return user_heap
+        if profile:
+            content = []
+
+            if "aboutSummaryText" in profile and profile["aboutSummaryText"]:
+                content.append(profile["aboutSummaryText"])
+
+            for p in linkedIn.getUserPosts(profile):
+                if "text" in p and p["text"]:
+                    content.append(p["text"])
+
+            content = "\n\n------------------\n".join(content)
+            saleNavId = linkedIn.getSaleNavId(profile["salesNavLink"])
+            username = linkedIn.getUsername(profile["link"])
+
+            user = {"id": saleNavId, "name": profile["name"], "username": username}
+
+            return content, user
+        return None, None
+
+    def scour(self, starting_users, query, user_limit):
+        user_heap = []
+
+        accounts = deque(starting_users)
+
+        visited = set()
+        count = 0
+
+        while accounts and count < user_limit:
+            account = accounts.popleft()
+            if (
+                "@" in account
+            ):  # If it contains @ it is mastodon otherwise it is LinkedIn URL
+                _, acc, server = account.split("@")
+                content, user = self.__get_mastodon_user(acc, server)
+
+                # If there is no user found, no point in excuting the rest of the code
+                if not user:
+                    continue
+
+                # Get mastodon followers
+                for follower in mastodon.getFollowers(server, user["id"]):
+                    username = follower["acct"]
+                    if "@" in username:
+                        username = "@" + username
+                    else:
+                        username = "@" + username + "@" + server
+                    if username not in visited:
+                        accounts.append(username)
+                        visited.add(username)
+            else:
+                content, user = self.__get_linkedIn_user(account)
+
+                # Get followers for linkedIn
+                for follower in linkedIn.getConnections(account, 1000):
+                    username = follower["publicIdentifier"]
+                    if username not in visited:
+                        accounts.append(username)
+                        visited.add(username)
+
+            if user:
+                try:
+                    score = llm_server.generate_search(query, content)["response"]
+                    self.__store_items(
+                        ((int(score), account, user)), user_limit, user_heap
+                    )
+                    # print(count)
+                    count += 1
+                except requests.exceptions.RequestException as e:
+                    raise e
+                except Exception as e:
+                    raise Exception("Error encountered on storing the scores")
+
+        return user_heap
