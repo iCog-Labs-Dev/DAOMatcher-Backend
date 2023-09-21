0d71f362a6040cd819415d9e89b25ff6c17a16b8
Yab <yabuman46@gmail.com>
2023-09-21 13:11:05 +0300

    Adjusted the setup script to be suitable for deployement

--- setup.sh
+++ setup.sh
@@ -16,49 +16,8 @@ cleanup() {
     exit 0
 }
 
-# Function to check if Redis is installed
-check_redis_installed() {
-    if [ -x "$(command -v redis-server)" ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Function to install Redis
-install_redis() {
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        # macOS using Homebrew
-        brew install redis -q
-    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux using APT (Debian/Ubuntu)
-        sudo apt-get update -q
-        sudo apt-get install -y redis-server
-    else
-        echo "Unsupported operating system: $OSTYPE install redis-server manually and try running this script again"
-        exit 1
-    fi
-}
-
 
 trap cleanup SIGINT
-
-echo "Setting up environment"
-
-python3 -m venv Backend
-source Backend/bin/activate     
-
-echo "Installing requirements"
-pip -q install -r requirements.txt
-
-# Check if Redis is installed
-if check_redis_installed; then
-    echo "Redis is already installed."
-else
-    echo "Redis is not installed. Installing Redis..."
-    install_redis
-    wait
-fi
 
 echo "Starting LLM server on port 5001"
 python3 -m src.LLM.LLMServer & 
@@ -68,10 +27,5 @@ echo "Starting App server on port 8000"
 source Backend/bin/activate
 gunicorn 'app:create_app()' --worker-class gevent --bind 127.0.0.1:8000 &
 app_pid=$!
-
-# Start Redis server
-echo "Starting Redis server..."
-redis-server --port 5002 &
-redis_pid=$!
 
 wait
