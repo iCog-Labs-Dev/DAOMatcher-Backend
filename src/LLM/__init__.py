aea4717cefd68c1b2f4a21aa515b45fc40c1ff26
Yab <yabuman46@gmail.com>
2023-10-25 15:54:47 +0300

    Changed the prompt to work relatively better

--- src/LLM/__init__.py
+++ src/LLM/__init__.py
@@ -12,16 +12,16 @@ DEFAULT_SYSTEM_PROMPT = """
   You are expert topic interest inferer. Your task is to provide an interest of a human on a given topic on the scale of 1-100. Your response output should contain only one number that coresponds to the interest of the person on the topic.
   If the user seems to be extremly likely to be interested in the topic output 100. If the user doesn't seem to be likely to be interested at all output 0.
   You should never provide any explantion to the output, your response should be only in numbers corresponding to the scores, nothing else.
+
+  topic: <{query}>
+  Description: ```{content}```
   """
-INSTRUCTION = """For the following topic in '<>' and posts separated by '------------------'  in '``````' perform the following steps
+INSTRUCTION = """For the above topic in '<>' and posts separated by '------------------'  in '``````' perform the following steps
   step 1: summarize each post that is separated by '------------------' at most 50 words.
   step 2: from the summarized posts above, identify sentences that are related to the given topic.
   step 3: if you can find sentences related to the provided topics procced with step 3 other wise output 0 and end your analysis.
   step 4: rate how much interested the user can be on the given topic on the scale of 1-100 based on the identified sentences and their correlation with the topic.
   step 5: ouput a single number that is precise and detailed with a variety of ranges that is between 1-100 based on step 4.
-
-  topic: <{query}>
-  Description: ```{content}```
 
   Strictly provide your output as follows. Don't include any explanation or any text.
   Response: <single number from 1-100>
