709838853199eb01034baf6dc38f74cd51714d0a
Yab <yabuman46@gmail.com>
2023-11-01 14:53:48 +0300

    Bug fix: For not getting profile of a user in a different server

--- src/Scraping/Mastodon.py
+++ src/Scraping/Mastodon.py
@@ -10,15 +10,34 @@ class Mastodon:
 
     # These function specific to Mastodon Scraping
 
-    def getProfile(self, server, acc):
-        # BASE_URL = MASTEDON_BASE_URL.format(server=server)
-        BASE_URL = MASTEDON_BASE_URL
-        acc = "@".join([acc, server])
+    def getProfile(self, server, acc, webfinger=False, searchServer="mastodon.social"):
+        # Change the searching server if webfinger is enabled
+        BASE_URL = MASTEDON_BASE_URL.format(
+            server=searchServer if webfinger else server
+        )
         try:
-            print(f"{BASE_URL}/lookup?acct=@{acc}")
-            response = requests.get(f"{BASE_URL}/lookup?acct=@{acc}")
+            # Setting the search param based on the type of search
+            # When webfinger is True, mastodon will search for the user's server from the current server itself. If not it will just try to find the user itself from the server
+            acc = "@" + "@".join([acc, server]) if webfinger else acc
+            print(f"{BASE_URL}/lookup?acct={acc}")
+            response = requests.get(f"{BASE_URL}/lookup?acct={acc}")
             response.raise_for_status()
             return response.json()
+        except requests.exceptions.RequestException as errh:
+            print(f"\033[91;1m{errh}.\033[0m\n")
+            print(f"\033[94;1mRetrying with a different Server...\033[0m\n")
+            if (
+                (errh.response != None)
+                and (errh.response.status_code == 404)
+                and (not webfinger)
+            ):
+                return self.getProfile(
+                    server=server,
+                    acc=acc,
+                    webfinger=True,
+                    searchServer="mastodon.social",
+                )
+            return False
         except Exception as e:
             print(f"\033[91;1m{e}.\033[0m\n")
             return False
