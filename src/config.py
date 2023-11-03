7ca1095bfd324e7560a07d2ecca0998c7a1df299
Yab <yabuman46@gmail.com>
2023-11-03 05:30:22 +0300

    Secret key not setting error handled

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ class Config:
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = SECRET_KEY if SECRET_KEY else "This is a secret key"
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_SAMESITE = "None"
 
