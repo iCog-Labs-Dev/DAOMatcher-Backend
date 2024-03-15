9233877ac426aa9b6dc6a553b8efbf66398c200e
Yab <yabuman46@gmail.com>
2024-03-15 13:05:41 +0300

    chore: Added local server url for development

--- src/extensions.py
+++ src/extensions.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_socketio import SocketIO
 from src.globals import FRONTEND_URL
 
-origins = [FRONTEND_URL if FRONTEND_URL else "http://localhost:5173"]
+origins = [FRONTEND_URL, "http://localhost:5173"]
 
 socketio = SocketIO(cors_allowed_origins=FRONTEND_URL if FRONTEND_URL else "*")
 login_manager = LoginManager()
