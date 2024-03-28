5907887111808a159e73f2741eba0490e0a668a9
Yab <yabuman46@gmail.com>
2024-03-28 09:59:16 +0300

    chore: Configured alembic

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from src.config import DevelopmentConfig, Config
 from src import prod_env
-from src.extensions import login_manager, socketio, cors, db
+from src.extensions import login_manager, socketio, cors, db, alembic
 from src.views import auth, main, error
 
 
@@ -13,6 +13,7 @@ def create_app():
     login_manager.init_app(app)
     socketio.init_app(app)
     db.init_app(app)
+    alembic.init_app(app)
 
     app.register_blueprint(main)
     app.register_blueprint(auth)
--- src/extensions.py
+++ src/extensions.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO
 from src import Base
 from src.globals import FRONTEND_URL
 from flask_sqlalchemy import SQLAlchemy
+from flask_alembic import Alembic
 
 
 origins = [FRONTEND_URL, "http://localhost:5173"]
@@ -12,3 +13,4 @@ socketio = SocketIO(cors_allowed_origins=origins)
 login_manager = LoginManager()
 cors = CORS(supports_credentials=True, origins=origins)
 db = SQLAlchemy(model_class=Base)
+alembic = Alembic()
