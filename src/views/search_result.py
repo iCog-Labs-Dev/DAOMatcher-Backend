a4e657be917d90bf5aef46b0b24c53a89783aff7
Yab <yabuman46@gmail.com>
2024-03-28 20:28:56 +0300

    feat: Implemented view functions

---
+++ src/views/search_result.py
@@ -0,0 +1,32 @@
+from src.controllers.search_result import *
+from flask import Blueprint
+
+search = Blueprint("search-result", __name__)
+
+
+@search.route("/", methods=["POST"])
+def add_search():
+    user_id = request.args.get("id")
+    search_result = add_search_result(user_id)
+    return search_result
+
+
+@search.route("/", methods=["GET"])
+def get_search():
+    search_id = request.json.get("id")
+    result = get_search_result(search_id)
+    return result
+
+
+@search.route("/", methods=["GET"])
+def get_searches():
+    user_id = request.json.get("id")
+    results = get_search_results(user_id)
+    return results
+
+
+@search.route("/usage", methods=["DELETE"])
+def delete_search():
+    search_id = request.json.get("id")
+    result = delete_search_result(search_id)
+    return result
---
+++ src/views/user.py
@@ -0,0 +1,33 @@
+from src.controllers.user import *
+from flask import Blueprint
+
+user = Blueprint("user", __name__)
+
+
+@user.route("/", methods=["GET"])
+def get_by_id():
+    user_id = request.args.get("id")
+    user = get_user_by_id(user_id)
+
+    return user
+
+
+@user.route("/", methods=["PUT"])
+def update():
+    user_id = request.args.get("id")
+    user = update_user(user_id)
+
+    return user
+
+
+@user.route("/", methods=["POST"])
+def create():
+    user = add_user()
+    return user
+
+
+@user.route("/usage", methods=["PUT"])
+def update_usage():
+    user_id = request.args.get("id")
+    updated_usage = update_user_usage(user_id)
+    return updated_usage
