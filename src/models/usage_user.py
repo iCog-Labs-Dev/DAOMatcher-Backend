6c7ea8bf2d7371af93c427114a89c502ca187fdf
Yab <yabuman46@gmail.com>
2024-03-28 10:50:12 +0300

    fix: Removed foreign key contraint

--- src/models/usage_user.py
+++ src/models/usage_user.py
@@ -7,5 +7,3 @@ class UserUsage(db.Model):
     id: Mapped[str] = mapped_column(String(length=50), primary_key=True)
     token_count: Mapped[int] = mapped_column(Integer, nullable=False)
     search_count: Mapped[int] = mapped_column(Integer, nullable=False)
-
-    user_id: Mapped[str] = mapped_column(String(length=50), ForeignKey("users.id"))
