136dcc8ca6f08697a283342510b3f53f74b43449
Yab <yabuman46@gmail.com>
2024-03-28 12:27:31 +0300

    fix: Minor fix of not found import

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from src.models.user import User
 from src.models.user_usage import UserUsage
 from src.models.user_setting import UserSetting
-from src.models.search_result import SearchResult, Username
+from src.models.search_result import SearchResult
+from src.models.username import Username
 from src.models.search_usernames import search_usernames
