0691a28f1221a166fe11bfad6a99b9dd3a43ac87
Yab <yabuman46@gmail.com>
2024-03-28 11:14:49 +0300

    feat: Added a type to the many to many table
    Used an Enum type to the many to many table instead of creating two different tables for basically similar purposes

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,6 @@
 from src.models.user import User
-
 from src.models.usernames import Usernames
 from src.models.usage_user import UserUsage
 from src.models.user_settings import UserSettings
 from src.models.search_result import SearchResult
-from src.models.many_to_many_tables import seed_usernames, found_usernames
+from src.models.many_to_many_tables import search_usernames
--- src/models/many_to_many_tables.py
+++ src/models/many_to_many_tables.py
@@ -1,15 +1,17 @@
-from sqlalchemy import ForeignKey, Column
+from enum import Enum as PEnum
+from sqlalchemy import ForeignKey, Column, Enum
 from src.extensions import db
 from src.models import SearchResult, Usernames
 
-seed_usernames = db.Table(
+
+class UsernameType(PEnum):
+    SEED = "seed"
+    FOUND = "found"
+
+
+search_usernames = db.Table(
     "seed_usernames",
     Column("search_id", ForeignKey(SearchResult.id)),
     Column("username_id", ForeignKey(Usernames.id)),
-)
-
-found_usernames = db.Table(
-    "found_usernames",
-    Column("search_id", ForeignKey(SearchResult.id)),
-    Column("username_id", ForeignKey(Usernames.id)),
+    Column("type", Enum(UsernameType)),
 )
