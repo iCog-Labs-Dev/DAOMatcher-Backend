f0921fb0032f8b19390dac91dbcca7db5c271b30
Yab <yabuman46@gmail.com>
2024-04-04 08:45:41 +0300

    refactor: Changed router structure in search result

--- src/views/search_result.py
+++ src/views/search_result.py
@@ -5,29 +5,24 @@ from src.controllers.search_result import *
 search = Blueprint("search-result", __name__)
 
 
-@search.route("/", methods=["POST"])
-def add_search():
-    user_id = request.args.get("id")
+@search.route("/<string:user_id>", methods=["POST"])
+def add_search(user_id):
     search_result = add_search_result(user_id)
     return search_result
 
 
-@search.route("/", methods=["GET"])
-def get_search():
-    search_id = request.json.get("id")
-    result = get_search_result(search_id)
-    return result
+@search.route("/search-result/<string:id>", methods=["GET"])
+def get_search(id):
+    if request.method == "GET":
+        result = get_search_result(id)
+        return result
+    elif request.method == "DELETE":
+        result = delete_search_result(id)
+        return result
 
 
-@search.route("/", methods=["GET"])
+@search.route("/search-result", methods=["GET"])
 def get_searches():
     user_id = request.json.get("id")
     results = get_search_results(user_id)
     return results
-
-
-@search.route("/usage", methods=["DELETE"])
-def delete_search():
-    search_id = request.json.get("id")
-    result = delete_search_result(search_id)
-    return result
