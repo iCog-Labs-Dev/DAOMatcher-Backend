09504c60c04fbc4fb676dd611b5cb6b6b3f11c69
samwondim <samwondim@gmail.com>
2024-04-29 15:21:36 +0300

    add updated migration files

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,13 @@
+import os
 from dotenv import load_dotenv
+from decouple import Config
+from sqlalchemy.orm import DeclarativeBase
 
+config = Config(os.path.join(os.path.pardir, ".env"))
 load_dotenv()
+
+prod_env = config("PROD_ENV", default=False)
+
+
+class Base(DeclarativeBase):
+    pass
