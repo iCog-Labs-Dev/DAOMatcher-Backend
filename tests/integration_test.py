e7eb6970f4efec7e2baf4cb21d0211d43d5f8409
BekLg <bereketlegesse22@gmail.com>
2024-06-11 13:24:03 +0300

    feat: added test for get_user

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -25,6 +25,33 @@ def test_login():
     pass
 
 
+def login():
+    payload = {
+        "email": "test@Daomatcher.io",
+        "password": "test@Daomatcher!"
+    }
+    response = requests.post(ENDPOINT + "/api/auth/login", json=payload)
+    data = response.json()
+    
+    token = data['data']['token']
+    user_info = data['data']['user']
+    user_id = user_info['id']
+
+    return {
+        "user_id": user_id,
+        "token": token
+    }
+
+def test_get_user():
+    auth_data=login()
+
+    headers = {
+    "Authorization": f"Bearer {auth_data['token']}"
+    }
+    get_user_response= requests.get(ENDPOINT + f"/api/user/{auth_data['user_id']}", headers=headers)
+    assert get_user_response.status_code == 200
+    pass
+    
 
 
     
