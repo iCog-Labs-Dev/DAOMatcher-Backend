b3fc7bcbecf9cd73f2e3f54e25184eee75abb59c
BekLg <bereketlegesse22@gmail.com>
2024-06-11 12:27:26 +0300

    feat: added a test for signup

---
+++ tests/integration_test.py
@@ -0,0 +1,17 @@
+import requests
+import uuid
+
+ENDPOINT= "http://localhost:8000"
+
+
+def test_sign_up():
+    email= uuid.uuid4().hex
+    payload= {
+    "display_name":"test user",
+	"email": f"{email}test@Daomatcher.io",
+	"password": "test@Daomatcher!"
+    }
+    sign_up_response= requests.post(ENDPOINT + "/api/auth/register", json=payload)
+    assert sign_up_response.status_code == 200
+    pass
+
