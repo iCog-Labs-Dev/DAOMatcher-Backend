b5b9c384625fd3e6058b6afd1f51b9cbab5720d2
BekLg <bereketlegesse22@gmail.com>
2024-06-11 14:12:34 +0300

    feat: added a test for update_user

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -4,6 +4,36 @@ import uuid
 ENDPOINT= "http://localhost:8000"
 
 
+def login():
+    payload = {
+        "email": "test@Daomatcher.io",
+        "password": "test@Daomatcher!"
+    }
+    response = requests.post(ENDPOINT + "/api/auth/login", json=payload)
+    data = response.json()
+    
+    token = data['data']['token']
+    user_info = data['data']['user']
+    user_id = user_info['id']
+
+    return {
+        "user_id": user_id,
+        "token": token
+    }
+
+def get_user():
+    auth_data=login()
+    headers = {
+    "Authorization": f"Bearer {auth_data['token']}"
+    }
+    response= requests.get(ENDPOINT + f"/api/user/{auth_data['user_id']}", headers=headers)
+    
+    data= response.json()
+    return data
+
+
+
+
 def test_sign_up():
     email= uuid.uuid4().hex
     payload= {
@@ -25,26 +55,9 @@ def test_login():
     pass
 
 
-def login():
-    payload = {
-        "email": "test@Daomatcher.io",
-        "password": "test@Daomatcher!"
-    }
-    response = requests.post(ENDPOINT + "/api/auth/login", json=payload)
-    data = response.json()
-    
-    token = data['data']['token']
-    user_info = data['data']['user']
-    user_id = user_info['id']
-
-    return {
-        "user_id": user_id,
-        "token": token
-    }
 
 def test_get_user():
     auth_data=login()
-
     headers = {
     "Authorization": f"Bearer {auth_data['token']}"
     }
@@ -53,5 +66,30 @@ def test_get_user():
     pass
     
 
+def test_update_user():
+    auth_data=login()
+    headers = {
+    "Authorization": f"Bearer {auth_data['token']}"
+    }
+
+    payload={
+        "api_key": "test_as87d6a987da987sd98a7sd98a7sd987df986df76876ef8"
+    }
+    update_user_response= requests.put(ENDPOINT + f"/api/user/{auth_data['user_id']}", headers=headers, json=payload)
+    assert update_user_response.status_code == 200, f"Expected status code 200 but got {update_user_response.status_code}"
+
+
+     # Get the updated user info
+    updated_user_data = get_user()
+    if not updated_user_data:
+        print("Failed to retrieve updated user data.")
+        return
+
+    # Compare the updated attribute
+    assert updated_user_data['data']['api_key'] == payload['api_key'], (
+        f"Expected api_key to be '{payload['api_key']}' but got '{updated_user_data['data']['api_key']}'"
+    )
+    pass
+    
 
     
